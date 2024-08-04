11123dcd232e8f96d789c267db63fca410061585
mbrown-2 <michaelbrown.5619@gmail.com>
2024-08-04 10:20:51 -0700

    Update

--- marketplace/src/App.tsx
+++ marketplace/src/App.tsx
@@ -1,8 +1,29 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Show } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
+      <Grid
+        templateAreas={{
+          // Define layouts based on screen size (i.e. mobile vs. PC)
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem area={"nav"} bg="Cornsilk">
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem area={"aside"} bg="DimGray" textColor="white">
+            Aside
+          </GridItem>
+        </Show>
+        <GridItem area={"main"} bg="#2F4F4F" textColor="white">
+          Main
+        </GridItem>
+      </Grid>
+
       <Button colorScheme="blue">Button</Button>
     </>
   );
