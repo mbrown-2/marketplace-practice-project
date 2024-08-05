ded440cf33cf7f1292d998ea7c5b64cd3273c509
mbrown-2 <michaelbrown.5619@gmail.com>
2024-08-04 20:37:06 -0700

    Modular code update. Controller integrated with initial api call.

--- marketplace/src/Components/GameGrid.tsx
+++ marketplace/src/Components/GameGrid.tsx
@@ -1,40 +1,16 @@
-import React, { useEffect, useState } from "react";
 import { Text } from "@chakra-ui/react";
-import apiClient from "../services/api-client";
-
-interface Game {
-  id: number;
-  name: string;
-  release: string;
-  tba: boolean;
-  background_image: string;
-  rating: number;
-  ratings_count: number;
-}
-
-interface FetchGamesResponse {
-  id: number;
-  count: number;
-  next: string;
-  previous: string;
-  results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  // Use EffectHook to call upon first render to make GET request
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
-  }, []);
+  const { games, error } = useGames();
 
   return (
     <>
-      {error && <Text>Encountered problem: {error}</Text>}
+      {error && (
+        <Text>
+          <b>Encountered problem:</b> {error}
+        </Text>
+      )}
       <ul>
         {games.map((game) => (
           <li key={game.id}>{game.name}</li>
