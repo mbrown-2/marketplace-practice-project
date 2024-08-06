8bb208cfee77bf1872a5c75eb75250acbdba4114
mbrown-2 <michaelbrown.5619@gmail.com>
2024-08-05 20:46:40 -0700

    Changed image sizes

--- marketplace/src/Components/GameCard.tsx
+++ marketplace/src/Components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Game } from "../hooks/useGames";
 import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   game: Game;
@@ -11,7 +12,7 @@ const GameCard = ({ game }: Props) => {
   return (
     <>
       <Card borderRadius={15} overflow="hidden">
-        <Image src={game.background_image} />
+        <Image src={getCroppedImageUrl(game.background_image)} />
         <CardBody>
           <Heading fontSize="2xl">{game.name}</Heading>
           <Text>Release date: {game.released}</Text>
