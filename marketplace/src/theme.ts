03f79d3899996be7599078598f5a74f44d7f57e8
mbrown-2 <michaelbrown.5619@gmail.com>
2024-08-10 19:01:12 -0700

    custom dark-theme (maroon/red style)

--- marketplace/src/theme.ts
+++ marketplace/src/theme.ts
@@ -7,6 +7,21 @@ const config: ThemeConfig = {
 };
 
 // Extend the theme.
-const theme = extendTheme({ config });
+const theme = extendTheme({ config,
+    colors: {
+        gray: {
+            50: "#f9f9f9",
+            100: "#ededed",
+            200: "#d3d3d3",
+            300: "#b3b3b3",
+            400: "#a0a0a0",
+            500: "#898989",
+            600: "#6c6c6c",
+            700: "#3b111c",
+            800: "#1b0108",
+            900: "#111"
+        }
+    }
+ });
 
 export default theme;
