0a5f20f376dd75461afbe2483f8a9a666fc99eee
mbrown-2 <michaelbrown.5619@gmail.com>
2024-08-11 16:04:03 -0700

    fixes

--- marketplace/src/services/api-client.ts
+++ marketplace/src/services/api-client.ts
@@ -1,11 +1,10 @@
 import axios from "axios";
-import { defineConfig, loadEnv } from "vite"
 // import connection from "./apikey"
 
 
 export default axios.create({
     baseURL: "https://api.rawg.io/api",
     params: {
-        key: import.meta.env.API_KEY
+        key:  import.meta.env.VITE_API_TOKEN
     }
 })
