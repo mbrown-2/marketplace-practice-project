f78012a0d2c665c4dfb17a277621adefc7c1a3f7
mbrown-2 <michaelbrown.5619@gmail.com>
2024-08-18 22:53:19 -0700

    fixes

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -2,11 +2,14 @@ import { HStack, Image } from "@chakra-ui/react";
 import SiteLogo from "../assets/site-logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import { SearchBar } from "./SearchBar";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <HStack padding="15px">
-      <Image src={SiteLogo} objectFit={"cover"} boxSize="100px" />
+      <Link to="/">
+        <Image src={SiteLogo} objectFit={"cover"} boxSize="60px" />
+      </Link>
       <SearchBar />
       <ColorModeSwitch />
     </HStack>
